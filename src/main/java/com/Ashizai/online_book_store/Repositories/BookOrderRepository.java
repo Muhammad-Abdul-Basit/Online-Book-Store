9326c5ec33c21868c0aa774fcf9ba6ad9f203575
Muhammad-Abdul-Basit <271muhammadabdulbasit@gmail.com>
2025-04-06 21:00:41 +0500

    Repository layer almost completed, if anything required will be added as per recommendation

--- src/main/java/com/Ashizai/online_book_store/Repositories/BookOrderRepository.java
+++ src/main/java/com/Ashizai/online_book_store/Repositories/BookOrderRepository.java
@@ -2,8 +2,12 @@ package com.Ashizai.online_book_store.Repositories;
 import com.Ashizai.online_book_store.model.BookOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface BookOrderRepository extends JpaRepository <BookOrder, String>
 {
-
+    List<BookOrder>findByOrderDate(LocalDate orderDate);
 }
--- src/main/java/com/Ashizai/online_book_store/Repositories/CustomerRepository.java
+++ src/main/java/com/Ashizai/online_book_store/Repositories/CustomerRepository.java
@@ -2,8 +2,11 @@ package com.Ashizai.online_book_store.Repositories;
 import com.Ashizai.online_book_store.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository <Customer, String>
 {
-
+    List<Customer>findByCustomerName (String customerName);
 }
